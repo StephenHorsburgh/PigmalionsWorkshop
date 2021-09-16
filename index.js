a8f187d5845a062f2cc07071d5605606cc5259ae
Stephen Horsburgh <88470715+StephenHorsburgh@users.noreply.github.com>
2021-09-16 22:53:36 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const gameScreen = document.getElementById('gameScreen');
 
 const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
-const FOOD_COLOUR = 'e669916'; 
+const FOOD_COLOUR = '#e669916'; 
 
 const gameState = {
     player:{
@@ -14,9 +14,20 @@ const gameState = {
             x:1,
             y: 0,
 
-        }
-    }
-}
+        },
+        snake:[
+            {X: 1, y: 10},
+            {X: 2, y: 10},
+            {X: 3, y: 10},
+        ],
+    },
+    food:{
+        x:7,
+        y:7,
+    },
+    gridsize: 20,
+    
+};
 
 let canvas, ctx;
 
@@ -33,11 +44,33 @@ function init(){
 }
 
 function keydown(e){
-    if(keydown>1){
-        ctx.fillStyle += '#FF00FF';
-    }
+    
     console.log(e.keyCode);
     console.log(ctx.fillStyle);
 }
 
 init();
+
+function paintGame(state){
+    ctx.fillStyle = BG_COLOUR;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const food = state.food;
+    const gridsize = state.gridsize;
+    const size = canvas.width / gridsize;
+
+    ctx.fillStyle = FOOD_COLOUR;
+    ctx.fillRect(food.x * size, food.y * size, size, size);
+
+    paintPlayer(state.player, size, SNAKE_COLOUR);
+}
+
+function paintPlayer(playerState, size, colour){
+    const snake = playerState.snake;
+
+    for(let cell of snake){
+        ctx.fillRect(cell.x * size, cell.y * size, size, size);
+    }
+}
+
+paintGame(gameState);
